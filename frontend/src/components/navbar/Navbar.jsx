bb9722ac575197263d1fc032a9de97f9aa3ffd31
Kuba072002 <jakub072002@gmail.com>
2023-04-16 12:50:38 +0200

    Added sign out

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import {MdLogout} from "react-icons/md"; 
 import "./navbar.css"
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const logOut = async (e) => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
   return (
-    <nav className='navbar gradient_bg'>
+    <nav className='navbar bg'>
       <div className='navbar-links'>
         <div className='navbar-links_logo'>
-          <Logo  fill="#767a7c" />
+          <Logo fill="#767a7c" />
         </div>
         <div className='navbar-links_container'>
           <p><Link to="/">Home</Link></p>
           <p><Link to="/profile">Profile</Link></p>
         </div>
+      </div>
+      <div className='navbar_account'>
+        <button onClick={(e) => logOut(e)}>
+          Sign out
+          <MdLogout color={"#fff"} size={24} />
+        </button>
       </div>
     </nav>
   )
