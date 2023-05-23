022064a6941daee6b874dabb85bff0c69935b3ef
Kuba072002 <jakub072002@gmail.com>
2023-05-24 00:38:01 +0200

    added likes and comments to post service

---
+++ BackEnd/BackEnd/Controllers/PostController.cs
@@ -0,0 +1,44 @@
+﻿using BackEnd.ModelsDto;
+using BackEnd.Services.PictureService;
+using BackEnd.Services.UserService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpPost("AddPost"), Authorize]
+        public async Task<ActionResult<string>> AddPost([FromForm] AddPostDto request)
+        {
+            var response = await _postService.addPost(request);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Message);
+        }
+
+        [HttpGet("GetUserPosts/{userId}"), Authorize]
+        public async Task<ActionResult<List<PostDto>>> GetUserPosts(int userId)
+        {
+            var response = await _postService.getUserPosts(userId);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Data);
+        }
+    }
+}
